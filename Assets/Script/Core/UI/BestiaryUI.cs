7c918e6d5e6cab9e2e895f147c0112e8960be21c
Rafi Adnan <rafiadnan990@gmail.com>
2026-07-02 17:37:24 +0700

    Update: UI Bestiary

--- Assets/Script/Core/UI/BestiaryUI.cs
+++ Assets/Script/Core/UI/BestiaryUI.cs
@@ -1,11 +1,34 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BestiaryUI : MonoBehaviour
 {
     public static BestiaryUI instance;
 
-    public TMP_Text textUI;
+    [System.Serializable]
+    public class FishSlot
+    {
+        public FishData fishData;
+
+        public Image image;
+
+        public Button button;
+    }
+
+    [Header("Fish Slots")]
+    public FishSlot[] fishSlots;
+
+    public Sprite unknownSprite;
+
+    [Header("Detail Panel")]
+    public GameObject detailPanel;
+
+    public Image detailImage;
+
+    public TMP_Text detailName;
+
+    public TMP_Text detailDescription;
 
     void Awake()
     {
@@ -15,16 +38,52 @@ public class BestiaryUI : MonoBehaviour
     void Start()
     {
         RefreshUI();
+
+        detailPanel.SetActive(false);
     }
 
     public void RefreshUI()
     {
-        textUI.text = "BESTIARY\n\n";
-
-        foreach (string fishName in BestiaryManager.instance.GetDiscoveredFishNames())
+        foreach (FishSlot slot in fishSlots)
         {
-            textUI.text += "- " + fishName + "\n";
+            bool discovered =
+                BestiaryManager.instance
+                .IsDiscovered(slot.fishData);
+
+            if (discovered)
+            {
+                slot.image.sprite =
+                    slot.fishData.icon;
+
+                slot.button.interactable = true;
+
+                slot.button.onClick.RemoveAllListeners();
+
+                slot.button.onClick.AddListener(() =>
+                {
+                    ShowDetail(slot.fishData);
+                });
+            }
+            else
+            {
+                slot.image.sprite =
+                    unknownSprite;
+
+                slot.button.interactable = false;
+            }
         }
+    }
+
+    public void ShowDetail(FishData fish)
+    {
+        detailPanel.SetActive(true);
+
+        detailImage.sprite = fish.icon;
+
+        detailName.text = fish.fishName;
+
+        detailDescription.text =
+            fish.description;
     }
 
 }
